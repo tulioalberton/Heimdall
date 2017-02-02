f8a4704df2a90c8ce7f5f2a462d5738ab513c2a8
root <root@s11.wan>
2017-02-02 19:05:09 +0000

    New testes with improved code. Better results.

---
+++ tests/improved_1.0/scriptWT_1_Heimdall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 1 -Z 1 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S1-Z1.logCbench 2>&1
+sleep 15
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 1 -Z 2 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S1-Z2.logCbench 2>&1
+sleep 15
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 1 -Z 4 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S1-Z4.logCbench 2>&1
+sleep 15
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 1 -Z 8 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S1-Z8.logCbench 2>&1
+sleep 15
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 1 -Z 16 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S1-Z16.logCbench 2>&1
+sleep 15
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 2 -Z 16 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S2-Z16.logCbench 2>&1
+sleep 15
+/opt/mtcbench/oflops/cbench/cbench -c 192.168.3.33 -p 6653 -l 105 -M 32 -m 10000 -D 1000 -S 4 -Z 16 -e 12000 -w 5 -t > Heimdall_WT_1-L105-S4-Z16.logCbench 2>&1
